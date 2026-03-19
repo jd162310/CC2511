3031e60bb1edd203e9b0af9fb7402754f1fd75ca
jd162310 <bjay.mcmullen@my.jcu.edu.au>
2026-03-20 08:19:41 +1000

    Steps 1-5 on lab done

--- W4STARTM/main.c
+++ W4STARTM/main.c
@@ -8,11 +8,73 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-int main(void) {
-  // TODO - Initialise components and variables
-  while (true) {
-    // TODO - Repeated code here
-    printf("OUR REAL WORK\n");
-    return 0;
+// global variables
+int high_delay_us = 1; // microseconds
+int low_delay_us = 1; // microseconds 
+
+// Function for pin initialization
+int init_stepper_pins() {
+  // Define pins
+  #define STEP 14
+  #define DIR 15
+  #define ENABLE 22
+  // set the pins to output
+  gpio_init(ENABLE);
+  gpio_set_dir(ENABLE, GPIO_OUT);
+  gpio_put(ENABLE, 1);
+  gpio_init(STEP);
+  gpio_init(DIR);
+  gpio_set_dir(STEP, GPIO_OUT);
+  gpio_set_dir(DIR, GPIO_OUT);
+}
+
+// Function to send pulse signal to stepper motor
+int send_pulse_to_stepper() {
+  gpio_put(STEP, 1); // Step
+  sleep_us(high_delay_us); // Short delay
+  gpio_put(STEP, 0); // Clear step
+  sleep_us(low_delay_us); // Short delay
+}
+
+// Function to execute a number of steps
+int execute_n_steps(int steps) {
+  for (int i = 0; i < steps; i++) {
+    send_pulse_to_stepper();
   }
+}
+int main(void) {
+
+  stdio_init_all(); 
+
+  init_stepper_pins(); // initalizes pins inside main() with function call
+
+  while (true) {
+    // obtains user input
+    int c = getchar_timeout_us(0);
+    if (c != PICO_ERROR_TIMEOUT) {
+      switch(c) {
+          case 'f':
+          case 'F':
+          execute_n_steps(1);
+          break;
+          case 'g':
+          case 'G':
+          execute_n_steps(10);
+          break;
+          case 'h':
+          case 'H':
+          execute_n_steps(100);
+          break;
+          case 'j':
+          case 'J':
+          execute_n_steps(200);
+          break;
+          default:
+          printf("Invalid inout");
+      }
+      
+
+  }
+}
+  return 0;
 }
