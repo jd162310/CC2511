219060cdbeb59416153014deaeb55b74904940e0
jd162310 <bjay.mcmullen@my.jcu.edu.au>
2026-03-19 13:38:39 +1000

    THE BEGINNING OF WEEK 4
    real keep it real guys dont cry

---
+++ W4STARTM/main.c
@@ -0,0 +1,18 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 19-Mar-2026 bjays
+ * W4STARTM
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+    printf("OUR REAL WORK\n");
+    return 0;
+  }
+}
