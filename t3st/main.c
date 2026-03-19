be3683e4913f379edafa951a9d88c2bff1234909
jd162310 <bjay.mcmullen@my.jcu.edu.au>
2026-03-19 13:35:38 +1000

    testing required

--- t3st/main.c
+++ t3st/main.c
@@ -1,16 +1,19 @@
 /**************************************************************
  * main.c
- * rev 1.0 03-Feb-2026 bjays
- * –help
+ * rev 1.0 19-Mar-2026 bjays
+ * t3st
  * ***********************************************************/
 
 #include "pico/stdlib.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 
 int main(void) {
   // TODO - Initialise components and variables
   while (true) {
+    printf("samuel\n");
     // TODO - Repeated code here
+    return 0;
   }
 }
