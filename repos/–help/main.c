3f9d768d690e7aed62a5436e75df2dfae8a511ed
jd162310 <bjay.mcmullen@my.jcu.edu.au>
2026-03-19 11:59:22 +1000

    b

---
+++ repos/–help/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 03-Feb-2026 bjays
+ * –help
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
