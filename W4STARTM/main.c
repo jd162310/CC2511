1b6f93834bca345fb45d6fb1bddc120ae5dcd68d
jd162310 <bjay.mcmullen@my.jcu.edu.au>
2026-03-20 19:34:21 +1000

    Update main.c
    Updated stepper motor control with x, y, and z control with modes. It requires more control options to control each axis separately

--- W4STARTM/main.c
+++ W4STARTM/main.c
@@ -10,12 +10,21 @@
 
 // global variables
 // Define pins
-  #define STEP 5
-  #define DIR 6
+  #define X_STEP 5
+  #define X_DIR 6
+  #define Y_STEP 11
+  #define Y_DIR 12
+  #define Z_STEP 14
+  #define Z_DIR 15
   #define ENABLE 22
-  #define Mode0 2
-  #define Mode1 3
-  #define Mode2 4
+  // Mode pins for microstepping
+  #define X_Mode0 2
+  #define X_Mode1 3
+  #define X_Mode2 4
+  #define Y_Mode0 8
+  #define Y_Mode1 9
+  #define Y_Mode2 10
+
 int high_delay_us = 1000; // microseconds
 int low_delay_us = 1000; // microseconds
 
@@ -25,24 +34,45 @@ void init_stepper_pins() {
   gpio_init(ENABLE);
   gpio_set_dir(ENABLE, GPIO_OUT);
   gpio_put(ENABLE, 0);
-  gpio_init(STEP);
-  gpio_init(DIR);
-  gpio_set_dir(STEP, GPIO_OUT);
-  gpio_set_dir(DIR, GPIO_OUT);
-  gpio_init(Mode0);
-  gpio_init(Mode1); 
-  gpio_init(Mode2);
-  gpio_set_dir(Mode0, GPIO_OUT);
-  gpio_set_dir(Mode1, GPIO_OUT);
-  gpio_set_dir(Mode2, GPIO_OUT);
+  // X stepper motor pins
+  gpio_init(X_STEP);
+  gpio_init(X_DIR);
+  gpio_set_dir(X_STEP, GPIO_OUT);
+  gpio_set_dir(X_DIR, GPIO_OUT);
+  gpio_init(X_Mode0);
+  gpio_init(X_Mode1); 
+  gpio_init(X_Mode2);
+  gpio_set_dir(X_Mode0, GPIO_OUT);
+  gpio_set_dir(X_Mode1, GPIO_OUT);
+  gpio_set_dir(X_Mode2, GPIO_OUT);
+  // y stepper motor pins
+  gpio_init(Y_STEP);
+  gpio_init(Y_DIR);
+  gpio_set_dir(Y_STEP, GPIO_OUT);
+  gpio_set_dir(Y_DIR, GPIO_OUT);
+  gpio_init(Y_Mode0);
+  gpio_init(Y_Mode1); 
+  gpio_init(Y_Mode2);
+  gpio_set_dir(Y_Mode0, GPIO_OUT);
+  gpio_set_dir(Y_Mode1, GPIO_OUT);
+  gpio_set_dir(Y_Mode2, GPIO_OUT);
+  // z stepper motor pins
+  gpio_init(Z_STEP);
+  gpio_init(Z_DIR);
+  gpio_set_dir(Z_STEP, GPIO_OUT);
+  gpio_set_dir(Z_DIR, GPIO_OUT);
 }
 
 // Function to send pulse signal to stepper motor
 void send_pulse_to_stepper() {
-  gpio_put(STEP, 1); // Step
-  sleep_us(high_delay_us); // Short delay
-  gpio_put(STEP, 0); // Clear step
-  sleep_us(low_delay_us); // Short delay
+  gpio_put(X_STEP, 1); 
+  gpio_put(Y_STEP, 1);
+  gpio_put(Z_STEP, 1);
+  sleep_us(high_delay_us); 
+  gpio_put(X_STEP, 0);
+  gpio_put(Y_STEP, 0);
+  gpio_put(Z_STEP, 0); 
+  sleep_us(low_delay_us); 
 }
 
 // Function to execute a number of steps
@@ -54,41 +84,62 @@ void execute_n_steps(int steps) {
 
 // function to set the direction of the stepper motor
 void set_stepper_direction(bool forward) {
-  gpio_put(DIR, forward); // sets direction forward or backward based on user input
+  // sets direction forward or backward based on user input
+  gpio_put(X_DIR, forward); 
+  gpio_put(Y_DIR, forward);
+  gpio_put(Z_DIR, forward);
 }
 
 // function to set up microstepping mode
 void set_microstepping_mode(int mode) {
   switch(mode) {
     case 1: // full step
-      gpio_put(Mode0, 0);
-      gpio_put(Mode1, 0);
-      gpio_put(Mode2, 0);
+      gpio_put(X_Mode0, 0);
+      gpio_put(X_Mode1, 0);
+      gpio_put(X_Mode2, 0);
+      gpio_put(Y_Mode0, 0);
+      gpio_put(Y_Mode1, 0);
+      gpio_put(Y_Mode2, 0);
       break;
     case 2: // half step
-      gpio_put(Mode0, 1);
-      gpio_put(Mode1, 0);
-      gpio_put(Mode2, 0);
+      gpio_put(X_Mode0, 1);
+      gpio_put(X_Mode1, 0);
+      gpio_put(X_Mode2, 0);
+      gpio_put(Y_Mode0, 1);
+      gpio_put(Y_Mode1, 0);
+      gpio_put(Y_Mode2, 0);
       break;
     case 4: // quarter step
-      gpio_put(Mode0, 0);
-      gpio_put(Mode1, 1);
-      gpio_put(Mode2, 0);
+      gpio_put(X_Mode0, 0);
+      gpio_put(X_Mode1, 1);
+      gpio_put(X_Mode2, 0);
+      gpio_put(Y_Mode0, 0);
+      gpio_put(Y_Mode1, 1);
+      gpio_put(Y_Mode2, 0);
       break;
     case 8: // eighth step
-      gpio_put(Mode0, 1);
-      gpio_put(Mode1, 1);
-      gpio_put(Mode2, 0);
+      gpio_put(X_Mode0, 1);
+      gpio_put(X_Mode1, 1);
+      gpio_put(X_Mode2, 0);
+      gpio_put(Y_Mode0, 1);
+      gpio_put(Y_Mode1, 1);
+      gpio_put(Y_Mode2, 0);
       break;
     case 16: // sixteenth step
-      gpio_put(Mode0, 1);
-      gpio_put(Mode1, 1);
-      gpio_put(Mode2, 1);
+      gpio_put(X_Mode0, 1);
+      gpio_put(X_Mode1, 1);
+      gpio_put(X_Mode2, 1);
+      gpio_put(Y_Mode0, 1);
+      gpio_put(Y_Mode1, 1);
+      gpio_put(Y_Mode2, 1);
       break;
     case 32: // thirty-second step
-      gpio_put(Mode0, 1);
-      gpio_put(Mode1, 0);
-      gpio_put(Mode2, 1);
+      gpio_put(X_Mode0, 1);
+      gpio_put(X_Mode1, 0);
+      gpio_put(X_Mode2, 1);
+      gpio_put(Y_Mode0, 1);
+      gpio_put(Y_Mode1, 0);
+      gpio_put(Y_Mode2, 1);
       break;    
       default:
       printf("Invalid microstepping mode");
