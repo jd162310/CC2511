d42811654a91ec92285c34bb09e26c6993b8213e
jd162310 <bjay.mcmullen@my.jcu.edu.au>
2026-03-20 12:53:42 +1000

    Update main.c
    Full starter motor code with modes

--- W4STARTM/main.c
+++ W4STARTM/main.c
@@ -17,7 +17,7 @@
   #define Mode1 3
   #define Mode2 4
 int high_delay_us = 1000; // microseconds
-int low_delay_us = 1000; // microseconds 
+int low_delay_us = 1000; // microseconds
 
 // Function for pin initialization
 void init_stepper_pins() {
@@ -29,6 +29,12 @@ void init_stepper_pins() {
   gpio_init(DIR);
   gpio_set_dir(STEP, GPIO_OUT);
   gpio_set_dir(DIR, GPIO_OUT);
+  gpio_init(Mode0);
+  gpio_init(Mode1); 
+  gpio_init(Mode2);
+  gpio_set_dir(Mode0, GPIO_OUT);
+  gpio_set_dir(Mode1, GPIO_OUT);
+  gpio_set_dir(Mode2, GPIO_OUT);
 }
 
 // Function to send pulse signal to stepper motor
@@ -49,6 +55,44 @@ void execute_n_steps(int steps) {
 // function to set the direction of the stepper motor
 void set_stepper_direction(bool forward) {
   gpio_put(DIR, forward); // sets direction forward or backward based on user input
+}
+
+// function to set up microstepping mode
+void set_microstepping_mode(int mode) {
+  switch(mode) {
+    case 1: // full step
+      gpio_put(Mode0, 0);
+      gpio_put(Mode1, 0);
+      gpio_put(Mode2, 0);
+      break;
+    case 2: // half step
+      gpio_put(Mode0, 1);
+      gpio_put(Mode1, 0);
+      gpio_put(Mode2, 0);
+      break;
+    case 4: // quarter step
+      gpio_put(Mode0, 0);
+      gpio_put(Mode1, 1);
+      gpio_put(Mode2, 0);
+      break;
+    case 8: // eighth step
+      gpio_put(Mode0, 1);
+      gpio_put(Mode1, 1);
+      gpio_put(Mode2, 0);
+      break;
+    case 16: // sixteenth step
+      gpio_put(Mode0, 1);
+      gpio_put(Mode1, 1);
+      gpio_put(Mode2, 1);
+      break;
+    case 32: // thirty-second step
+      gpio_put(Mode0, 1);
+      gpio_put(Mode1, 0);
+      gpio_put(Mode2, 1);
+      break;    
+      default:
+      printf("Invalid microstepping mode");
+  }
 }
 int main(void) {
 
@@ -84,6 +128,24 @@ int main(void) {
           case 'a':
           case 'A':
           set_stepper_direction(false);
+          break;
+          case '0':
+          set_microstepping_mode(1);
+          break;
+          case '1':
+          set_microstepping_mode(2);
+          break;
+          case '2':
+          set_microstepping_mode(4);
+          break;
+          case '3':
+          set_microstepping_mode(8);
+          break;
+          case '4':
+          set_microstepping_mode(16);
+          break;
+          case '5':
+          set_microstepping_mode(32);
           break;
           default:
           printf("Invalid inout");
